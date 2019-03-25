680d5707292583bad494ee7a239a21cd2e47e00b
lijiateng <joseph_l@126.com>
2019-03-25 08:21:32 +0800

    FlexibleGridDecoration

---
+++ app/src/main/java/com/ljt/rvanalysis/basic/decorations/FlexibleGridDecoration.java
@@ -0,0 +1,13 @@
+package com.ljt.rvanalysis.basic.decorations;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by lijiateng on 2019/3/24.
+ */
+
+public class FlexibleGridDecoration extends RecyclerView.ItemDecoration {
+
+
+
+}
